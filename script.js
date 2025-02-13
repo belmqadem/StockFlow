19c95b685a759cb46a59463fcfabf08ef0c3f46c
Adil Bel Mqadem <abel-mqa@e2r5p6.1337.ma>
2025-02-13 16:46:47 +0100

    its done, still some enhacements

--- script.js
+++ script.js
@@ -1,119 +1,61 @@
-let body = document.body;
-let title = document.getElementById("title");
-let price = document.getElementById("price");
-let taxes = document.getElementById("taxes");
-let discount = document.getElementById("discount");
-let total = document.getElementById("total");
-let count = document.getElementById("count");
-let category = document.getElementById("category");
-let btnAdd = document.getElementById("btn-add");
-let table = document.getElementById("table");
-let search = document.getElementById("search");
-let btnSearchTitle = document.getElementById("btn-search-title");
-let btnSearchCategory = document.getElementById("btn-search-category");
-let btnDelete = document.getElementById("btn-delete");
+const title = document.getElementById("title");
+const price = document.getElementById("price");
+const taxes = document.getElementById("taxes");
+const discount = document.getElementById("discount");
+const total = document.getElementById("total");
+const count = document.getElementById("count");
+const category = document.getElementById("category");
+const btnAdd = document.getElementById("btn-add");
+const table = document.getElementById("product-table-body");
+const search = document.getElementById("search");
+const btnSearchTitle = document.getElementById("btn-search-title");
+const btnSearchCategory = document.getElementById("btn-search-category");
+const btnDelete = document.getElementById("delete");
+const scroll = document.getElementById("scroll");
 
-localStorage.clear();
+// localStorage.clear();
 
-const saveToLocalStorage = () => {
-  localStorage.setItem("title", title.value.trim());
-  localStorage.setItem("price", price.value.trim());
-  localStorage.setItem("taxes", taxes.value.trim());
-  localStorage.setItem("discount", discount.value.trim());
-  localStorage.setItem("count", count.value.trim());
-  localStorage.setItem("category", category.value.trim());
-  localStorage.setItem("total", total.value);
-};
-
-const updateTotal = () => {
-  if (
-    price.value.trim() === "" ||
-    taxes.value.trim() === "" ||
-    discount.value.trim() === ""
-  ) {
+// Function to calculate total
+function updateTotal() {
+  if (price.value == "" || taxes.value == "" || discount.value == "") {
     total.value = "";
     total.style.background = "rgba(204, 0, 0, 0.35)";
-    localStorage.setItem("total", "");
     return;
   }
   let priceValue = parseFloat(price.value) || 0;
   let taxesValue = parseFloat(taxes.value) || 0;
   let discountValue = parseFloat(discount.value) || 0;
-
   let totalValue = priceValue + taxesValue - discountValue;
   total.value = totalValue >= 0 ? totalValue : 0;
   total.style.background = "#EEEFF2";
-  localStorage.setItem("total", total.value);
-};
-
-title.addEventListener("input", saveToLocalStorage);
-price.addEventListener("input", () => {
-  saveToLocalStorage();
-  updateTotal();
-});
-taxes.addEventListener("input", () => {
-  saveToLocalStorage();
-  updateTotal();
-});
-discount.addEventListener("input", () => {
-  saveToLocalStorage();
-  updateTotal();
-});
-count.addEventListener("input", saveToLocalStorage);
-category.addEventListener("input", saveToLocalStorage);
-
-function addItem(itemObj) {
-  let row = document.createElement("tr");
-  let itemValues = [
-    itemObj.id,
-    itemObj.title,
-    itemObj.price,
-    itemObj.taxes,
-    itemObj.discount,
-    itemObj.total,
-    itemObj.category,
-  ];
-  itemValues.forEach((value) => {
-    let column = document.createElement("td");
-    column.textContent = value;
-    row.appendChild(column);
-  });
-
-  let updateColumn = document.createElement("td");
-  let updateBtn = document.createElement("button");
-  updateBtn.textContent = "UPDATE";
-  updateBtn.className = "btn-secondary";
-  updateBtn.id = "btn-update";
-  updateColumn.appendChild(updateBtn);
-  row.appendChild(updateColumn);
-
-  let deleteColumn = document.createElement("td");
-  let deleteBtn = document.createElement("button");
-  deleteBtn.textContent = "DELETE";
-  deleteBtn.className = "btn-secondary";
-  deleteBtn.id = "btn-delete-one";
-  deleteColumn.appendChild(deleteBtn);
-  row.appendChild(deleteColumn);
-
-  table.appendChild(row);
 }
 
-let index = localStorage.getItem("index")
-  ? parseInt(localStorage.getItem("index"))
-  : 0;
+// Event listeners to update total dinamically
+price.addEventListener("input", updateTotal);
+taxes.addEventListener("input", updateTotal);
+discount.addEventListener("input", updateTotal);
 
+// Init product array from local storage
+let products = JSON.parse(localStorage.getItem("product")) || [];
+
+// Function to add products
 btnAdd.onclick = () => {
-  let itemCount = parseInt(count.value) || 1;
+  if (parseInt(count.value) <= 0) {
+    alert("You're trying to add 0 elements");
+    return;
+  }
+  const itemCount = parseInt(count.value) || 1;
+
   for (let i = 0; i < itemCount; i++) {
     let item = {
-      id: index,
-      title: title.value,
-      price: price.value,
-      taxes: taxes.value,
-      discount: discount.value,
-      total: total.value,
-      category: category.value,
+      title: title.value.trim(),
+      price: price.value.trim(),
+      taxes: taxes.value.trim(),
+      discount: discount.value.trim(),
+      total: total.value.trim(),
+      category: category.value.trim(),
     };
+
     if (
       item.title === "" ||
       item.price === "" ||
@@ -121,20 +63,122 @@ btnAdd.onclick = () => {
       item.discount === "" ||
       item.category === ""
     ) {
+      alert("All fields must be filled.");
       return;
     }
-    addItem(item);
-    index++;
+    products.push(item);
+    localStorage.setItem("product", JSON.stringify(products));
   }
-  localStorage.setItem("index", index);
+
+  [title, price, taxes, discount, count, category].forEach(
+    (input) => (input.value = "")
+  );
+  updateTotal();
+  showProduct();
 };
 
-function filterTableBy(columnIndex) {
+// function that diisplays the delete button
+function showDeleteButton() {
+  btnDelete.style.display = "block";
+  btnDelete.innerHTML = `delete all (${products.length})`;
+}
+
+// Function that shows the products in the table
+function showProduct() {
+  let rows = "";
+  for (let i = 0; i < products.length; i++) {
+    rows += `
+		<tr>
+			<td>${i}</td>
+			<td>${products[i].title}</td>
+			<td>${products[i].price}</td>
+			<td>${products[i].taxes}</td>
+			<td>${products[i].discount}</td>
+			<td>${products[i].total}</td>
+			<td>${products[i].category}</td>
+			<td><button class="btn-secondary btn-update" onclick="updateOneProduct(${i})">update</button></td>
+			<td><button class="btn-secondary btn-delete" onclick="deleteOneProduct(${i})">delete</button></td>
+		</tr>
+	`;
+  }
+  table.innerHTML = rows;
+  if (products.length > 0) {
+    showDeleteButton();
+  }
+}
+
+// SHow data from local storage
+showProduct();
+
+// Function to delete all data from table
+btnDelete.addEventListener("click", () => {
+  table.innerHTML = "";
+  localStorage.clear();
+  products = [];
+  btnDelete.style.display = "none";
+});
+
+// function to delete one product from table
+function deleteOneProduct(index) {
+  products.splice(index, 1);
+  localStorage.setItem("product", JSON.stringify(products));
+  showProduct();
+}
+
+// function to update one product
+function updateOneProduct(index) {
+  title.value = products[index].title;
+  price.value = products[index].price;
+  taxes.value = products[index].taxes;
+  discount.value = products[index].discount;
+  total.value = products[index].total;
+  category.value = products[index].category;
+
+  count.setAttribute("readonly", null);
+  btnAdd.textContent = "update product";
+  btnAdd.onclick = () => {
+    saveUpdatedProduct(index);
+  };
+}
+
+// function to save the updates for product
+function saveUpdatedProduct(index) {
+  if (
+    title.value == "" ||
+    price.value == "" ||
+    taxes.value == "" ||
+    discount.value == "" ||
+    category.value == ""
+  ) {
+    alert("Enter some values");
+    return;
+  }
+  products[index] = {
+    title: title.value.trim(),
+    price: price.value.trim(),
+    taxes: taxes.value.trim(),
+    discount: discount.value.trim(),
+    total: total.value.trim(),
+    category: category.value.trim(),
+  };
+
+  localStorage.setItem("product", JSON.stringify(products));
+
+  btnAdd.textContent = "add product";
+  [title, price, taxes, discount, total, category].forEach(
+    (input) => (input.value = "")
+  );
+  showProduct();
+  count.removeAttribute("readonly");
+}
+
+// Function that filters table
+function filterTableBy(index) {
   let filter = search.value.trim().toLowerCase();
   let rows = table.getElementsByTagName("tr");
 
-  for (let i = 1; i < rows.length; i++) {
-    let cell = rows[i].getElementsByTagName("td")[columnIndex];
+  for (let i = 0; i < rows.length; i++) {
+    let cell = rows[i].getElementsByTagName("td")[index];
     if (cell) {
       let text = cell.textContent.trim().toLowerCase();
       rows[i].style.display = text.includes(filter) ? "" : "none";
@@ -143,9 +187,31 @@ function filterTableBy(columnIndex) {
 }
 
 btnSearchTitle.addEventListener("focus", () => {
+  search.placeholder = "Search by Title...";
+  search.dataset.searchIndex = "1";
   filterTableBy(1);
 });
-
 btnSearchCategory.addEventListener("focus", () => {
+  search.placeholder = "Search by Category...";
+  search.dataset.searchIndex = "6";
   filterTableBy(6);
 });
+
+search.addEventListener("input", () => {
+  let index = search.dataset.searchIndex
+    ? parseInt(search.dataset.searchIndex)
+    : 1;
+  filterTableBy(index);
+});
+
+window.onscroll = () => {
+  if (window.scrollY >= 1000) {
+    scroll.style.display = "block";
+  } else {
+    scroll.style.display = "none";
+  }
+};
+
+scroll.onclick = () => {
+  window.scrollTo(0, 0);
+};
